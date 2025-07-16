44386a62682e71ac1667e64e48bd43e664f36b2d
AadeeshRS <srivastavaaadeesh@gmail.com>
2025-07-16 15:30:24 +0530

    Changed the generate page

--- app/shorten/page.js
+++ app/shorten/page.js
@@ -40,8 +40,8 @@ const Shorten = () => {
 
 
     return (
-        <div className='mx-auto max-w-lg bg-amber-100 my-10 p-5 rounded-xl flex flex-col gap-3.5'>
-            <h1 className='font-bold text-2xl text-center' >Generate your short URLs</h1>
+        <div className='mx-auto max-w-lg bg-amber-50 shadow my-10 p-5 rounded-xl flex flex-col gap-3.5'>
+            <h1 className='text-3xl font-bold text-green-900 mb-2' >Generate your short URLs</h1>
             <div className='flex flex-col gap-3'>
                 <input type="text" value={url} placeholder='Enter your URL' onChange={e => { seturl(e.target.value) }} className='bg-white focus:outline-green-300 p-2 px-4' />
                 <input type="text" value={shorturl} placeholder='Enter your preferred short URL' onChange={e => { setshorturl(e.target.value) }} className='bg-white focus:outline-green-300 p-2 px-4' />
